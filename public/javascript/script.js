6c630a2b990cdf6ef8d10b993f39890fb9abc6bb
rbs517 <rebeccaskurnik@gmail.com>
2022-02-04 02:42:24 +0200

    added intro pt1

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,17 +1,48 @@
 let previousSelection, modal;
+let intro = document.querySelector('.intro');
+let introContainer = document.querySelector('.intro-container');
+let introText = document.getElementById('text');
+let bodyWrapper = document.getElementById('body-wrapper');
 
 window.addEventListener('load', (event) => {
-  const moments = document.getElementsByClassName("gallery-item");
-  const infoBtn = document.getElementById("info-button");
-  modal = document.getElementById("modal");
-  const closeBtn = document.getElementById("close-button");
+  setTimeout(()=>{
+    introText.classList.add('active');
 
-  for(let i = 0; i < moments.length; i++) {
-    moments[i].addEventListener('click', element => expandSquare(element));
-  }
+    setTimeout(()=>{
+      introText.classList.remove('active');
+      introText.classList.add('fade');
+    }, 1300)
 
-  infoBtn.addEventListener('click', openModal);
-  closeBtn.addEventListener('click', closeModal);
+  }, 500)
+
+  setTimeout(()=>{
+    introText.innerHTML = "50 Moments";
+    introText.classList.remove('fade');
+    introText.classList.add('active');
+
+    setTimeout(()=>{
+      introText.classList.remove('active');
+      introText.classList.add('fade');
+
+      intro.style.visibility = 'hidden';
+      bodyWrapper.style.visibility = 'visible';
+
+      const moments = document.getElementsByClassName("gallery-item");
+      const infoBtn = document.getElementById("info-button");
+      modal = document.getElementById("modal");
+      const closeBtn = document.getElementById("close-button");
+    
+      for(let i = 0; i < moments.length; i++) {
+        moments[i].addEventListener('click', element => expandSquare(element));
+      }
+      
+      infoBtn.addEventListener('click', openModal);
+      closeBtn.addEventListener('click', closeModal);
+
+    }, 3200)
+
+  }, 2700)
+  
 });
 
 function expandSquare(e) {
@@ -34,9 +65,9 @@ function expandSquare(e) {
 }
 
 function openModal() {
-  modal.style.visibility = "visible";
+  modal.style.visibility = 'visible';
 }
 
 function closeModal() {
-  modal.style.visibility = "hidden";
+  modal.style.visibility = 'hidden';
 }
